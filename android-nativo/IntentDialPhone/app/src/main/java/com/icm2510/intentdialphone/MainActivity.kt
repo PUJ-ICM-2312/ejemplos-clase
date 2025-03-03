d5c9626eb1d71190fc71616e0b3a362ff759e987
Mauricio Cubides <mcubides@javeriana.edu.co>
2025-03-03 15:59:59 -0500

    Se elimina la validación de resolvedActivity para evitar conflicto.

--- android-nativo/IntentDialPhone/app/src/main/java/com/icm2510/intentdialphone/MainActivity.kt
+++ android-nativo/IntentDialPhone/app/src/main/java/com/icm2510/intentdialphone/MainActivity.kt
@@ -3,13 +3,14 @@ package com.icm2510.intentdialphone
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,16 +40,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
-    val context = LocalContext.current
     val phoneNumber = "+81 965-431-3024"
+    val context = LocalContext.current
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text("Número de teléfono: $phoneNumber")
-
+        Text(
+            text = "Número de teléfono: $phoneNumber",
+            style = MaterialTheme.typography.titleLarge)
+        Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
             dialPhoneNumber(context, phoneNumber)
         }) {
@@ -60,10 +64,5 @@ fun dialPhoneNumber(context: android.content.Context, phoneNumber: String) {
     val intent = Intent(Intent.ACTION_DIAL).apply {
         data = Uri.parse("tel:$phoneNumber")
     }
-
-    val resolvedActivity = intent.resolveActivity(context.packageManager)
-
-    if (intent.resolveActivity(context.packageManager) != null) {
-        context.startActivity(intent)
-    }
+    context.startActivity(intent)
 }
